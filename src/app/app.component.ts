98fefe2e80a53e27a10f5948c0fe3695cf55b971
Cristian Quispe <cquispe@belatrixsf.com>
2019-04-26 13:56:10 -0500

    change default style sheet to scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'ndl-root',
     templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css'],
+    styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
     title = 'ui-catalog';
