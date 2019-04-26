ba1798d7b31cece41708b4ecf66df3ee56cbcbe9
Cristian Quispe <cquispe@belatrixsf.com>
2019-04-26 16:03:48 -0500

    change panel component viewEncapsulation mode

--- src/app/panel/panel.component.ts
+++ src/app/panel/panel.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
     selector: 'ndl-panel',
     templateUrl: './panel.component.html',
     styleUrls: ['./panel.component.scss'],
+    encapsulation: ViewEncapsulation.ShadowDom,
 })
 export class PanelComponent implements OnInit {
     @Input() header: string;
